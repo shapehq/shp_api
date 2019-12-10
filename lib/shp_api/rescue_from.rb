385e1c6f1b3effc05e107c30e1261735a198f5cd
Ulrik Andersen <ulrik@shape.dk>
2019-12-10 16:57:49 +0100

    Log exceptions that are catched by ShpApi

--- lib/shp_api/rescue_from.rb
+++ lib/shp_api/rescue_from.rb
@@ -4,21 +4,28 @@ module ShpApi
     include ActiveSupport::Rescuable
     
     included do
-      
       # rescue_from are evaluated bottom-to-top so we rescue general
       # exception last.
       rescue_from Exception do |exception|
+        self.class.log_exception(exception)
         ShpApi::JsonResponder.new(self).exception(exception: exception)
       end
       
       rescue_from ActionController::ParameterMissing do |exception|
+        self.class.log_exception(exception)
         ShpApi::JsonResponder.new(self).param_missing(exception: exception)
       end
       
       rescue_from ActiveRecord::RecordNotFound do |exception|
+        self.class.log_exception(exception)
         ShpApi::JsonResponder.new(self).not_found(exception: exception)
       end
-      
+    end
+
+    module ClassMethods
+      def log_exception(exception)
+        logger.error((["ShpApi rescued from: #{exception.class} (#{exception.message})"]+exception.backtrace).join($/))
+      end
     end
   end
 end
